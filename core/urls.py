f790e1c9582ddb79e011bde169046dc083c9f098
Manish <timalsinamanish35@gmail.com>
2026-09-15 14:21:48 +0545

    Add patient add/edit/delete functionality

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("patients/", views.patient_list, name="patient_list"),
+    path("patients/add/", views.patient_add, name="patient_add"),
+    path("patients/<int:pk>/edit/", views.patient_edit, name="patient_edit"),
+    path("patients/<int:pk>/delete/", views.patient_delete, name="patient_delete"),
     path("appointments/", views.appointment_list, name="appointment_list"),
     path("emergency-queue/", views.emergency_queue_view, name="emergency_queue"),
 ]
--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from  .models import Patient,  Appointment
 from .emergency_queue import get_prority_queue
+from django.shortcuts import redirect
+from .forms import PatientForm
 
 def home(request):
         return render(request, "core/home.html")
@@ -17,3 +19,33 @@ def appointment_list(request):
 def emergency_queue_view(request):
         ordered_patients = get_prority_queue()
         return render(request, "core/emergency_queue.html", {"patients": ordered_patients})
+
+def patient_add(request):
+        if request.method == "POST":
+                form  = PatientForm(request.POST)
+                if form.is_valid():
+                        form.save()
+                        return redirect("patient_list")
+                else:
+                        form = PatientForm()
+                        return render(request, "core/patient_form.html", {"form": form})
+
+def patient_edit(request, pk):
+        patient = Patient.objects.get(id=pk)
+        if request.method == "POST":
+                form = PatientForm(request.POST, instance=patient)
+                if form.is_valid():
+                        form.save()
+                        return redirect("patient_list")
+        else:
+                        form = PatientForm(instance=patient)
+        return render(request, "core/patient_form.html", {"form": form})               
+
+def patient_delete(request, pk):
+        patient = Patient.objects.get(id=pk)
+        if  request.method == "POST":
+                patient.delete()
+                return redirect("patient_list")
+        return render(request, "core/patient_confirm_delete.html", {"patient": patient})                
+
+
